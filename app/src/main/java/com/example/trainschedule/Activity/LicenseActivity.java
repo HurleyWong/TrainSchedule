9374f0c8fd1f608cab8271ff0ee15c6421b7b051
Hurley Huang <15201218@stu.nchu.edu.cn>
2018-08-16 08:36:15 +0800

    为RecyclerView添加自定义分割线

--- app/src/main/java/com/example/trainschedule/Activity/LicenseActivity.java
+++ app/src/main/java/com/example/trainschedule/Activity/LicenseActivity.java
@@ -1,10 +1,10 @@
 package com.example.trainschedule.Activity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -13,6 +13,9 @@ import android.view.MenuItem;
 import com.example.trainschedule.Adapter.LicenseAdapter;
 import com.example.trainschedule.Model.License;
 import com.example.trainschedule.R;
+import com.yanyusong.y_divideritemdecoration.Y_Divider;
+import com.yanyusong.y_divideritemdecoration.Y_DividerBuilder;
+import com.yanyusong.y_divideritemdecoration.Y_DividerItemDecoration;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,6 +38,8 @@ public class LicenseActivity extends AppCompatActivity{
     private List<License> licenses=new ArrayList<>();
     private LicenseAdapter licenseAdapter;
 
+    private Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -45,7 +50,7 @@ public class LicenseActivity extends AppCompatActivity{
         setToolbar();
 
         //添加RecyclerView的数据
-        dealRecyclerView();
+        dealRecyclerView(context);
     }
 
     //初始化控件
@@ -84,18 +89,60 @@ public class LicenseActivity extends AppCompatActivity{
         return super.onOptionsItemSelected(item);
     }
 
-    private void dealRecyclerView(){
+    private void dealRecyclerView(Context context){
         //添加所使用的开源项目的名称和地址
         licenses.add(new License("Gson","https://github.com/google/gson"));
         licenses.add(new License("Volley","https://github.com/google/volley"));
         licenses.add(new License("PinchImageView","https://github.com/boycy815/PinchImageView"));
         licenses.add(new License("FlyShapeImageView","https://github.com/FlyRecker/FlyShapeImageView"));
+        licenses.add(new License("Y_DividerItemDecoration","https://github.com/yanyusong/Y_DividerItemDecoration"));
 
         licenseAdapter=new LicenseAdapter(this,licenses);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         //为RecyclerView添加分割线
-        recyclerView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this));
+        //recyclerView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
+        //默认添加分割线方式
+        //recyclerView.addItemDecoration(new RecyclerViewDivider(context,LinearLayoutManager.VERTICAL,R.drawable.divider_bg));
         recyclerView.setAdapter(licenseAdapter);
+    }
+
+    private class DividerItemDecoration extends Y_DividerItemDecoration{
+
+        private DividerItemDecoration(Context context){
+            super(context);
+        }
+
+        @Override
+        public Y_Divider getDivider(int itemPosition){
+            Y_Divider divider=null;
+            switch(itemPosition%2){
+                case 0:
+                    //每一行第一个显示right和bottom
+                    divider=new Y_DividerBuilder()
+                            //.setRightSideLine(true,0xff666666,10,0,0)
+                            .setBottomSideLine(true,0xff9e9e9e,1,0,0)
+                            .create();
+                    break;
+                case 1:
+                    //每一行第二个显示left和bottom
+                    divider=new Y_DividerBuilder()
+                            //.setLeftSideLine(true,0xff666666,10,0,0)
+                            .setBottomSideLine(true,0xff9e9e9e,1,0,0)
+                            .create();
+                    break;
+                default:
+                    break;
+            }
+            //第一行上方显示分割线
+            if(itemPosition==0){
+                divider=new Y_DividerBuilder()
+                        .setTopSideLine(true,0xff9e9e9e,1,0,0)
+                        .setBottomSideLine(true,0xff9e9e9e,1,0,0)
+                        .create();
+            }
+            return divider;
+        }
     }
 }
 
