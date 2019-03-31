2c3610577823d522a29e34a670300e4a93961bba
Hurley <1401682479@qq.com>
2019-03-31 21:03:06 +0800

    去除androidx

---
+++ app/src/main/java/com/example/trainschedule/base/BaseActivity.java
@@ -0,0 +1,15 @@
+package com.example.trainschedule.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * <pre>
+ *      @author hurley
+ *      date    : 2019/3/31 9:02 PM
+ *      github  : https://github.com/HurleyJames
+ *      desc    :
+ * </pre>
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+    
+}
